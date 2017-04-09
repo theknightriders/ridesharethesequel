b52bea8f9a9900837594139223f2ac25cd0366f6
myrddincat <myrddincat@gmail.com>
2017-04-09 12:21:15 -0400

    Very minor changes to fix a small mistake

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -11,22 +11,5 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 } 
-//echo "Connected successfully";
 
-// TESTING add data to a table (DOES NOT WORK YET)
-  /*
-  $sql = "INSERT INTO MyGuests (fname, lname, email)
-  VALUES ('John', 'Doe', 'john@example.com')";
-
-  if ($conn->query($sql) === TRUE) {
-      echo "New record created successfully";
-  } else {
-      echo "Error: " . $sql . "<br>" . $conn->error;
-  }
-  */
-
-
-
-// Close connection
-//$conn->close();
 ?>
